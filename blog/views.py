240d8a2ff6803cb3736db3dcceb09921950d8346
Arina345 <arishka.krikunova@gmail.com>
2023-12-02 21:45:01 +0300

    Завершение работы над внешним видом

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def home_page(request):
